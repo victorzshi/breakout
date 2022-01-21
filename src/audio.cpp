b9eafa8bba8b94940358e37dba61e57c61ccbcc0
Victor Z. Shi <vzshi@pm.me>
2022-01-20 20:15:31 -0500

    Add libraries (#13)
    * Rename assets folder to data
    
    * Add SDL libraries
    
    * Update README

--- src/audio.cpp
+++ src/audio.cpp
@@ -12,49 +12,49 @@ Audio::Audio()
 		throw;
 	}
 
-    beeep = Mix_LoadWAV("assets/sounds/sounds_ping_pong_8bit/ping_pong_8bit_beeep.ogg");
+    beeep = Mix_LoadWAV("data/sounds/sounds_ping_pong_8bit/ping_pong_8bit_beeep.ogg");
     if (beeep == NULL)
     {
         printf("Failed to load beeep sound effect! SDL_mixer Error: %s\n", Mix_GetError());
     }
 
-    peeeeeep = Mix_LoadWAV("assets/sounds/sounds_ping_pong_8bit/ping_pong_8bit_peeeeeep.ogg");
+    peeeeeep = Mix_LoadWAV("data/sounds/sounds_ping_pong_8bit/ping_pong_8bit_peeeeeep.ogg");
     if (peeeeeep == NULL)
     {
         printf("Failed to load peeeeeep sound effect! SDL_mixer Error: %s\n", Mix_GetError());
     }
 
-    plop = Mix_LoadWAV("assets/sounds/sounds_ping_pong_8bit/ping_pong_8bit_plop.ogg");
+    plop = Mix_LoadWAV("data/sounds/sounds_ping_pong_8bit/ping_pong_8bit_plop.ogg");
     if (plop == NULL)
     {
         printf("Failed to load plop sound effect! SDL_mixer Error: %s\n", Mix_GetError());
     }
 
-    death = Mix_LoadWAV("assets/sounds/8bit Death Whirl/vgdeathsound.ogg");
+    death = Mix_LoadWAV("data/sounds/8bit Death Whirl/vgdeathsound.ogg");
     if (death == NULL)
     {
         printf("Failed to load death sound effect! SDL_mixer Error: %s\n", Mix_GetError());
     }
 
-    flawless_victory = Mix_LoadWAV("assets/sounds/Voiceover Pack Fighter/flawless_victory.ogg");
+    flawless_victory = Mix_LoadWAV("data/sounds/Voiceover Pack Fighter/flawless_victory.ogg");
     if (flawless_victory == NULL)
     {
         printf("Failed to load flawless_victory sound effect! SDL_mixer Error: %s\n", Mix_GetError());
     }
 
-    winner = Mix_LoadWAV("assets/sounds/Voiceover Pack Fighter/winner.ogg");
+    winner = Mix_LoadWAV("data/sounds/Voiceover Pack Fighter/winner.ogg");
     if (winner == NULL)
     {
         printf("Failed to load winner sound effect! SDL_mixer Error: %s\n", Mix_GetError());
     }
 
-    you_win = Mix_LoadWAV("assets/sounds/Voiceover Pack Fighter/you_win.ogg");
+    you_win = Mix_LoadWAV("data/sounds/Voiceover Pack Fighter/you_win.ogg");
     if (you_win == NULL)
     {
         printf("Failed to load you_win sound effect! SDL_mixer Error: %s\n", Mix_GetError());
     }
 
-    game_over = Mix_LoadWAV("assets/sounds/Voiceover Pack Fighter/game_over.ogg");
+    game_over = Mix_LoadWAV("data/sounds/Voiceover Pack Fighter/game_over.ogg");
     if (game_over == NULL)
     {
         printf("Failed to load game_over sound effect! SDL_mixer Error: %s\n", Mix_GetError());
